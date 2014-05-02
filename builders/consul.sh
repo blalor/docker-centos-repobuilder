5c5291c4bd426ec339e0c55f09f4bd50ad897a53
Brian Lalor <blalor@bravo5.org>
2014-05-02 06:52:58 -0400

    WIP

--- builders/consul.sh
+++ builders/consul.sh
@@ -6,26 +6,37 @@ set -x
 . $( dirname $0 )/common
 
 tmpdir=$( mktemp -d )
-trap "echo removing ${tmpdir}; rm -rf ${tmpdir}" EXIT
+# trap "echo removing ${tmpdir}; rm -rf ${tmpdir}" EXIT
 
 PKG_NAME="consul"
 PKG_VER="0.2.0"
 PKG_ARCHIVE="${PKG_VER}_linux_amd64.zip"
 PKG_URL_BASE="https://dl.bintray.com/mitchellh/consul"
 
+SOURCES_DIR="${SOURCES}/${PKG_NAME}"
+
 if pkg_exists_in_repo ${PKG_NAME}-${PKG_VER}; then
     echo "${PKG_NAME}-${PKG_VER} already built"
 else
     pushd ${tmpdir}
     
-    mkdir -p usr/local
-    
     ## retrieve archive
     curl --remote-name-all --location ${PKG_URL_BASE}/${PKG_ARCHIVE}
     
+    ## create required directories
+    mkdir -p usr/bin etc/consul.d var/lib/consul etc/rc.d/init.d
+    
     ## unpack
     unzip ${PKG_ARCHIVE}
-    mv consul usr/local/
+    mv consul usr/bin/consul
+    chmod 555 usr/bin/consul
+
+    ## config file
+    cp "${SOURCES_DIR}/consul.conf" etc/consul.conf
+    
+    ## init script
+    cp "${SOURCES_DIR}/sysvinit.sh" etc/rc.d/init.d/consul
+    chmod 555 etc/rc.d/init.d/consul    
     
     popd
     fpm \
@@ -33,6 +44,13 @@ else
         -t rpm \
         -n ${PKG_NAME} \
         -v ${PKG_VER} \
+        --before-install "${SOURCES_DIR}/before-install.sh" \
+        --rpm-user consul \
+        --rpm-group consul \
+        --rpm-use-file-permissions \
+        --config-files /etc/consul.conf \
+        --directories /etc/consul.d \
+        --directories /var/lib/consul \
         -C ${tmpdir} \
-        usr/local
+        etc usr var
 fi
---
+++ sources/README.md
@@ -0,0 +1,2 @@
+Supporting files for builders that aren't able to retrieve all of what they need
+from some other canonical source.
